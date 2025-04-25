27b9a2273e83d8d445e1c03e8b431a478458a327
diguu0909 <digendraq@gmail.com>
2025-04-25 09:03:25 +0530

    Update Ex_2.2(bmi calculator ).py
    We have to make bmi calculator

--- Day_02/Ex_2.2(bmi calculator ).py
+++ Day_02/Ex_2.2(bmi calculator ).py
@@ -1,1 +1,18 @@
 
+weight=input("Write your weight(kg) :  ")
+height = input("write your height(m)  :  ")
+
+#input function always give string so convert in integer 
+
+X=int(weight)
+Y=int(height)
+
+bmi=str(X/Y**2)
+
+# we need to convert x/y**2 in str because we can't cacenate integer with string see next step'
+
+print("Your body mass index(bmi) is = " +bmi)
+
+#alternate
+#bmi=X/Y**2
+#print("Your body amss index(bmi) is = " + str(bmi))
