118d9e5b8e214eaf1ecddd648e57288ce6acede8
diguu0909 <digendraq@gmail.com>
2025-05-10 14:58:18 +0530

    Update Ex_3.1(odd or even).py

--- day_04/python.py
+++ day_04/python.py
@@ -1,1 +1,12 @@
-ruko
+# write a program to find given integer is even or odd
+
+
+Number = input("What is your number : ")
+remainder= int(Number)%2
+
+if remainder==0:
+  print("No is even ")
+
+else:
+  print("No is odd")
+  
